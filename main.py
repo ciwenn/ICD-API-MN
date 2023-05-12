813b929ebc47cdc58edb3de96cacd1dab9078f23
ciwenn <cindy.wen@stonybrook.edu>
2023-05-12 21:12:18 +0000

    added changes to main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from flask import Flask, request 
 import pandas as pd 
-
 df = pd.read_csv('./data/diagnoses2013.csv')
-
 app = Flask(__name__)
-
 @app.route('/', methods=["GET"])
 def home():
     return 'this is a API service for MN ICD code details'
-
 @app.route('/preview', methods=["GET"])
 def preview():
-    top10rows = df.head(1)
+    top10rows = df.head(20)
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=['GET'])
+@app.route('/icd/<value>', methods=["GET"])
 def icdcode(value):
+    #V12
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
     if len(filtered) <= 0:
@@ -31,8 +28,7 @@ def icdcode2(value, value2):
     if len(filtered2) <= 0:
         return 'There is nothing here'
     else: 
-        return filtered2.to_json(orient="records")    
-    
+        return filtered2.to_json(orient="records")
 
 if __name__ == '__main__':
     app.run(debug=True)
